2760c229e4b2c414162f9a35d8b383da74810279
hwndu <hwndu@fuckoff.org>
2017-02-04 06:04:18 -0500

    Updated time

--- time/generate_time.py
+++ time/generate_time.py
@@ -19,7 +19,7 @@ def main():
     # Create the time entry
     fe = fg.add_entry()
     fe.id('https://github.com/hwndu/hwndu-tools/blob/master/rss.xml')
-    fe.title(u"\U0001F340")
+    fe.title('$")
     fe.content('')
     fe.summary(u'It is currently Day 15 (Morning)')
     fe.link(href='https://youtu.be/dQw4w9WgXcQ', rel='alternate')
