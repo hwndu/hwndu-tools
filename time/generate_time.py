f55454484719da5f4a925a74e3eb3080c4b9cc58
hwndu <hwndu@fuckoff.org>
2017-02-04 06:17:10 -0500

    Updated time

--- time/generate_time.py
+++ time/generate_time.py
@@ -1,9 +1,26 @@
 import datetime
 from feedgen.feed import FeedGenerator
+import math
 
 def get_milli():
     m = int((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
-    return m
+    return float(m)
+
+def generate_time():
+    delta = (get_milli()-1484902680000.0)/86400000.0
+    day = math.floor(delta)
+    period = ""
+    pointdelta = delta - day
+    if pointdelta < 0.3:
+        period = "Morning"
+    elif pointdelta < 0.58:
+        period = "Afternoon"
+    elif pointdelta < 0.71:
+        period = "Evening"
+    else:
+        period = "Night"
+    time_string = "It is currently Day %d (%s)." % (day, period)
+    return time_string
 
 def main():
     # Create the feed
@@ -21,7 +38,7 @@ def main():
     fe.id('https://github.com/hwndu/hwndu-tools/blob/master/rss.xml')
     fe.title('There is a new day when the clock hits 3:58 AM (ET).')
     fe.content('')
-    fe.summary(u'<b>It is currently Day 15 (Morning)</b>')
+    fe.summary(generate_time())
     fe.link(href='https://youtu.be/dQw4w9WgXcQ', rel='alternate')
     fe.author(name='Hwndindu', email='hwndu@fuckoff.org')
 
