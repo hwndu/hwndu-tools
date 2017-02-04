83b4a4827b67aeea651354e0e35e40bb81b5f441
hwndu <hwndu@fuckoff.org>
2017-02-04 07:15:28 -0500

    Updated the link to point to shadilay

--- time/generate_time.py
+++ time/generate_time.py
@@ -39,7 +39,7 @@ def main():
     fe.title('There is a new day when the clock hits 3:58 AM (ET).')
     fe.content('')
     fe.summary(generate_time())
-    fe.link(href='https://youtu.be/dQw4w9WgXcQ', rel='alternate')
+    fe.link(href='https://youtu.be/sZJDYlxkM-c', rel='alternate')
     fe.author(name='Hwndindu', email='hwndu@fuckoff.org')
 
     # Write the feed to file
