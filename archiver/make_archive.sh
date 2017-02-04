52e10e051617c412b7d0853a65dd2f6884272321
hwndu <hwndu@fuckoff.org>
2017-02-04 18:15:24 -0500

    Update archive

--- archiver/make_archive.sh
+++ archiver/make_archive.sh
@@ -12,11 +12,11 @@ while read -r line; do
     CTHU="./thumbs/${Array[0]}.jpg"
     NVID="gs://hwndu-video-backups/video/${Array[0]}.mp4"
     NTHU="gs://hwndu-video-backups/thumbnail/${Array[0]}.jpg"
-    #wget "${Array[1]}" -O "${CVID}"
+    wget "${Array[1]}" -O "${CVID}"
     wget "${Array[2]}" -O "${CTHU}"
-    #gsutil cp "${CVID}" "${NVID}"
+    gsutil cp "${CVID}" "${NVID}"
     gsutil cp "${CTHU}" "${NTHU}"
-    #gsutil acl ch -u AllUsers:R "${NVID}"
+    gsutil acl ch -u AllUsers:R "${NVID}"
     gsutil acl ch -u AllUsers:R "${NTHU}"
     rm "${CVID}"
     rm "${CTHU}"
@@ -24,6 +24,8 @@ done <<< "$WORK"
 
 rmdir vids
 rmdir thumbs
+
+./update_index.sh
 
 git add .
 git commit -m "Update archive"
